96de7289b0f6d8b24bba9f452df145e3ba9661a8
Ganesh Jagdale <jagdaleganesh9545@gmail.com>
2022-08-24 16:57:05 +0530

    Added AWS Services

--- clouduniqueid/clouds/aws/__init__.py
+++ clouduniqueid/clouds/aws/__init__.py
@@ -8,8 +8,9 @@ logger = logging.getLogger(__name__)
 
 class AWSUniqueId:
     def get_unique_id(
-        self, service: str, resourceType: str, resource: str, parent: str = None,
-        accountId: str = None, region: str = None, partition: str = 'aws',
+        self, service: str, resourceType: str, resource: str, parent: str = None, groupId: str = None,
+        accountId: str = None, region: str = None, partition: str = 'aws', UUID: str = None,
+        launchConfigurationId: str = None, imageTag: str = None, loadBalancerId: str = None, listenerId: str = None,
     ) -> str:
 
         uniqueIds: Dict = unique_ids
@@ -33,6 +34,30 @@ class AWSUniqueId:
         elif 'parent' in uniqueIds[service][resourceType] and not parent:
             logger.error("AWS parent required")
             raise ValueError("Invalid parameters provided, AWS parent required")
+
+        elif 'UUID' in uniqueIds[service][resourceType] and not UUID:
+            logger.error("AWS UUID required")
+            raise ValueError("Invalid parameters provided, AWS UUID required")
+
+        elif 'groupId' in uniqueIds[service][resourceType] and not groupId:
+            logger.error("AWS groupId required")
+            raise ValueError("Invalid parameters provided, AWS groupId required")
+
+        elif 'launchConfigurationId' in uniqueIds[service][resourceType] and not launchConfigurationId:
+            logger.error("AWS launchConfigurationId required")
+            raise ValueError("Invalid parameters provided, AWS launchConfigurationId required")
+
+        elif 'imageTag' in uniqueIds[service][resourceType] and not imageTag:
+            logger.error("AWS imageTag required")
+            raise ValueError("Invalid parameters provided, AWS imageTag required")
+
+        elif 'loadBalancerId' in uniqueIds[service][resourceType] and not loadBalancerId:
+            logger.error("AWS loadBalancerId required")
+            raise ValueError("Invalid parameters provided, AWS loadBalancerId required")
+
+        elif 'listenerId' in uniqueIds[service][resourceType] and not listenerId:
+            logger.error("AWS listenerId required")
+            raise ValueError("Invalid parameters provided, AWS listenerId required")
 
         elif resource.replace(" ", "") == "" or not resource:
             logger.error("AWS resource required")
